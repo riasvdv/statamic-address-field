90f8c89b5bd99d92d726196cae71c37c8ce69b58
Rias <hey@rias.be>
2025-08-22 13:45:12 +0200

    wip

--- webpack.mix.js
+++
@@ -1,8 +0,0 @@
-let mix = require('laravel-mix');
-
-mix.js('resources/js/addon.js', 'dist/js').vue({
-    options: {
-        transformAssetUrls: false,
-    }
-});
-mix.copyDirectory('node_modules/leaflet/dist/images', 'dist/images');
